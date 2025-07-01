273d6088b5e34feabf1ebe16ee5031b3655b6ce3
y-sugawara0508 <sugawara.yuto@marble-corp.co.jp>
2025-07-01 13:37:01 +0900

    リポジトリの新規作成

---
+++ src/main/java/jp/co/sss/mySpringService/Tra03SpringProjectApplication.java
@@ -0,0 +1,13 @@
+package jp.co.sss.mySpringService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tra03SpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tra03SpringProjectApplication.class, args);
+	}
+
+}
